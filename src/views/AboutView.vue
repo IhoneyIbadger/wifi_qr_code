b2d0cb87ef7fc64951435146db11c97301725631
GurkenHans <nicoboerger63@gmail.com>
2022-09-01 22:46:27 +0200

    Design changes

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,9 +1,9 @@
 <template>
-  <main>
-    <h1>This is an about page</h1>
+  <main class="py-8 md:py-10">
+    <section class="max-w-prose mx-auto px-4 py-4 md:py-5">
+      <h1 class="text-4xl tracking-tight font-bold text-black mb-8">About</h1>
+      
+      <p></p>
+    </section>
   </main>
 </template>
-
-<style>
-
-</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -24,17 +24,19 @@ export default {
 </script>
 
 <template>
-  <main class="max-w-screen-xl mx-auto px-4 py-8 md:py-10">
-    <div class="mb-6">
-      <h1 class="text-4xl tracking-tight font-bold text-black mb-4">Wifi QR-code generator</h1>
+  <main class="py-8 md:py-10">
+    <section class="max-w-prose mx-auto px-4 py-4 md:py-5 text-center">
+      <h1 class="text-4xl tracking-tight font-bold text-black mb-2">WiFi QR Code Generator</h1>
 
-      <p>Generate a QR-code for your Wifi.</p>
-    </div>
+      <h2 class="text-xl tracking-tight font-bold text-black">Generate a QR-code for your Wifi.</h2>
+    </section>
 
-    <div class="grid grid-cols-1 md:grid-cols-2 gap-x-4 gap-y-8">
-      <WifiConfigForm @generate-qr-code="updateQrCodeData" />
+    <section class="max-w-screen-xl mx-auto px-4 py-4 md:py-5">
+      <div class="bg-gray-200 rounded-lg p-8 grid grid-cols-1 md:grid-cols-2 gap-x-4 gap-y-8">
+        <WifiConfigForm @generate-qr-code="updateQrCodeData" />
 
-      <QrCodePreview :qrCodeData="qrCodeData" />
-    </div>
+        <QrCodePreview :qrCodeData="qrCodeData" />
+      </div>
+    </section>
   </main>
 </template>
