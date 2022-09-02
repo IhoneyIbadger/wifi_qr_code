2e4b40504af013a987c5a3acc40167e23e097e0b
GurkenHans <nicoboerger63@gmail.com>
2022-09-02 09:39:20 +0200

    Added footer navigation

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,15 @@
 <script setup>
 import { RouterView } from 'vue-router'
 import HeaderNavigation from '@/components/HeaderNavigation.vue'
+import FooterNavigation from '@/components/FooterNavigation.vue'
 </script>
 
 <template>
   <HeaderNavigation />
 
   <RouterView />
+
+  <FooterNavigation />
 </template>
 
 <style scoped>
---
+++ src/components/FooterNavigation.vue
@@ -0,0 +1,11 @@
+<template>
+  <footer class="max-w-screen-xl w-full mx-auto mt-auto px-4 flex justify-between items-center h-16 text-gray-500">
+    <span class="italic text-xs">"QR Code" is a registered trademark of DENSO WAVE INCORPORATED</span>
+
+    <nav class="flex font-medium text-sm gap-x-6">
+      <RouterLink to="/imprint" class="hover:text-black">Imprint</RouterLink>
+      
+      <RouterLink to="/privacy" class="hover:text-black">Privacy</RouterLink>
+    </nav>
+  </footer>
+</template>
